c4b313c8bb81de39d1b3178b1592aa83e83fa12d
sthu <shutinghu2000@gmail.com>
2020-10-03 17:35:08 -0700

    landing page pre react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Grid } from "@material-ui/core";
+import Header from './Header';
+import { ReactComponent as Groceries } from './svg/groceries.svg';
+import { ReactComponent as Department } from './svg/department-stores.svg';
+import { ReactComponent as Restaurants } from './svg/restaurants.svg';
+import { ReactComponent as Spaces } from './svg/public-spaces.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    backgroundColor: 'transparent'
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello team Canary!
-        </p>
-      </header>
-    </div>
+    <Grid container direction="column" >
+      <Grid item>
+        <Header />
+      </Grid>
+
+      <Grid item container>
+        <Grid xs={false} s={2} />
+        <Grid xs={12} s={8}>
+        <div style={{
+              position: 'absolute', left: '50%', top: '60%',
+              transform: 'translate(-50%, -50%)',
+              textAlign: 'center'
+          }}>
+          <h1>
+            Welcome, <br></br>
+            where are you headed today?
+          </h1><br></br>
+
+          {/* GROCERIES */}
+          <IconButton>
+          <Card className={classes.root} elevation={0}>
+          <CardContent>
+            <Groceries /> 
+            <br></br>
+            <h3>
+              Groceries
+            </h3>
+          </CardContent>
+          </Card>
+          </IconButton>
+
+          {/* DEPARTMENT STORE */}
+          <IconButton>
+          <Card className={classes.root} elevation={0}>
+          <CardContent>
+            <Department /> 
+            <br></br>
+            <h3>
+              Department Store
+            </h3>
+          </CardContent>
+          </Card>
+          </IconButton>
+
+          {/* RESTAURANTS */}
+          <IconButton>
+          <Card className={classes.root} elevation={0}>
+          <CardContent>
+            <Restaurants /> 
+            <br></br>
+            <h3>
+              Restaurants
+            </h3>
+          </CardContent>
+          </Card>
+          </IconButton>
+
+          {/* PUBLIC SPACES */}
+          <IconButton>
+          <Card className={classes.root} elevation={0}>
+          <CardContent>
+            <Spaces /> 
+            <br></br>
+            <h3>
+              Public Spaces
+            </h3>
+          </CardContent>
+          </Card>
+          </IconButton>
+
+          </div>
+        </Grid>
+        <Grid xs={false} s={2} /> 
+      </Grid>
+      
+    </Grid>
+
+
   );
+
 }
 
 export default App;
---
+++ my-app/src/Header.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import { ReactComponent as Logo } from './svg/logo.svg';
+import { ReactComponent as SearchIcon } from './svg/search.svg';
+import { ReactComponent as ReviewBtn } from './svg/addrev.svg';
+import { IconButton } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  customizeToolbar: {
+    minHeight: 125,
+  },
+  canaryLogo: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    color: 'black',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 20
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+  review: {
+    '& svg': {
+        fontSize: 100
+      },
+    padding: theme.spacing(0, 5),
+  }
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  // for conditional background change later
+  const currCategoryBackground = 'groceries';
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" elevation={0}
+        style={{ backgroundColor: '#FFDDC8' }}>
+        <Toolbar className={classes.customizeToolbar}>
+            <IconButton className = {classes.canaryLogo} >
+                <Logo />
+            </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            {/* About */}
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon} >
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search location…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          <IconButton className={classes.review}>
+            <ReviewBtn />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}

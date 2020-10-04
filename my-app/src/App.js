1e1cf8e45d897a86ed731df510e2d87f3063782a
Saad <benattisaad@gmail.com>
2020-10-04 14:44:13 -0400

    firestore stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,28 +16,29 @@ import './App.css';
 // import CreateReview from './components/create-review';
 import MapView from './components/map-view';
 import CreateReview from './components/create-review';
+import * as firebase from 'firebase';
 
 
-
-var firebase = require("firebase/app");
+// var firebase = require("firebase/app");
 
   // Add the Firebase products that you want to use
   require("firebase/auth");
   require("firebase/firestore");
 
-  var firebaseConfig = {
-      apiKey: "AIzaSyCBTHB21J-4g4-GD86mShx5Sd8JztJiwuQ",
-      authDomain: "ivyhacks-ebbc6.firebaseapp.com",
-      databaseURL: "https://ivyhacks-ebbc6.firebaseio.com",
-      projectId: "ivyhacks-ebbc6",
-      storageBucket: "ivyhacks-ebbc6.appspot.com",
-      messagingSenderId: "793780521339",
-      appId: "1:793780521339:web:80799d16e5163782b015de",
-      measurementId: "G-PVSJQD49N5"
-  };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCBTHB21J-4g4-GD86mShx5Sd8JztJiwuQ",
+  authDomain: "ivyhacks-ebbc6.firebaseapp.com",
+  databaseURL: "https://ivyhacks-ebbc6.firebaseio.com",
+  projectId: "ivyhacks-ebbc6",
+  storageBucket: "ivyhacks-ebbc6.appspot.com",
+  messagingSenderId: "793780521339",
+  appId: "1:793780521339:web:80799d16e5163782b015de",
+  measurementId: "G-PVSJQD49N5"
+};
   
   if (firebase.apps.length === 0) {
-    firebase.initializeApp({});
+    firebase.initializeApp(firebaseConfig);
   }
   else {
 
@@ -47,17 +48,29 @@ var firebase = require("firebase/app");
 
 
 
-const useStyles = makeStyles({
+  const useStyles = makeStyles({
   root: {
     minWidth: 275,
     backgroundColor: 'transparent'
   },
 });
 
+
+
+async function getReviews() {
+
+  const reviews = firebase.firestore().collection('Reviews');
+  const reviews_doc = await reviews.get();
+  reviews_doc.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+  });
+
+}
+
 function App() {
   const classes = useStyles();
 
-  
+  getReviews();
   
   
 
